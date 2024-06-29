654b2a2fa10e5d77e8bfef5acd86d794188fd97f
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2024-06-29 18:17:05 -0500

    New approach to writing service

--- dev/dnd/install.clj
+++ dev/dnd/install.clj
@@ -12,11 +12,14 @@
         (str/replace "<:env>" env)
         (str/replace "<:alias>" alias))))
 
+(defn sudo-spit [path content]
+  (let [command (str "sh -c 'echo \"" content "\" > " path "'")]
+    (sh "sudo" "-S" "sh" "-c" command)))
+
 (defn -main []
   (println "Installing Service")
   (let [env (env/env! "ME_ENV")]
-    (spit "dnd.service" (service-str env))
-    (sh "sudo mv -f dnd.service /etc/systemd/system/dnd.service")
+    (sudo-spit "/etc/systemd/system/dnd.service" (service-str env))
     (sh "sudo systemctl daemon-reload")
     (sh "sudo systemctl start dnd")
     (System/exit 0)))
