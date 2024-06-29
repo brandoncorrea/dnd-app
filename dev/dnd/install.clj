0857d452124852dacc5ef8f74dd0b23f4c72ff90
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2024-06-29 18:07:34 -0500

    cljs and css can exist separately from install

--- dev/dnd/install.clj
+++ dev/dnd/install.clj
@@ -1,7 +1,5 @@
 (ns dnd.install
   (:require [c3kit.apron.env :as env]
-            [c3kit.scaffold.cljs :as cljs]
-            [c3kit.scaffold.css :as css]
             [clojure.java.shell :as shell]
             [clojure.string :as str]))
 
@@ -14,17 +12,11 @@
         (str/replace "<:env>" env)
         (str/replace "<:alias>" alias))))
 
-(defn- install-service! [env]
-  (println "Installing Service")
-  (spit "dnd.service" (service-str env))
-  (sh "sudo mv -f dnd.service /etc/systemd/system")
-  (sh "sudo systemctl daemon-reload")
-  (sh "sudo systemctl start dnd"))
-
 (defn -main []
-  (let [env (env/env "ME_ENV")]
-    (println "Installing Application:" env)
-    (cljs/-main "once")
-    (css/-main "once")
-    (install-service! env)
+  (println "Installing Service")
+  (let [env (env/env! "ME_ENV")]
+    (spit "dnd.service" (service-str env))
+    (sh "sudo mv -f dnd.service /etc/systemd/system")
+    (sh "sudo systemctl daemon-reload")
+    (sh "sudo systemctl start dnd")
     (System/exit 0)))
