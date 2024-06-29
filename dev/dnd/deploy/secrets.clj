5b44dba4bcc1d74abaf664b0404bda43184f5433
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2024-06-29 17:09:59 -0500

    Fix 'secrets' reference and inspect echo commanbds

--- dev/dnd/deploy/secrets.clj
+++ dev/dnd/deploy/secrets.clj
@@ -1,7 +1,8 @@
 (ns dnd.deploy.secrets
-  (:require [dnd.deploy.core :refer :all]))
+  (:require [c3kit.apron.corec :as ccc]
+            [dnd.deploy.core :refer :all]))
 
 (defn -main []
   (println "Installing Secrets")
   (doseq [k #{"ME_ENV" "HOSTNAME"}]
-    (sh (str "echo \"" k "=${{ secret." k " }}\" >> .env"))))
+    (sh (ccc/->inspect (str "echo \"" k "=${{ secrets." k " }}\" >> .env")))))
