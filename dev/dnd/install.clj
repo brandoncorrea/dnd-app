4b07673a8461dfc2e47e1db70ab6fd2d44fb24fb
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2024-06-29 18:34:58 -0500

    Everything is executed under a shell context

--- dev/dnd/install.clj
+++ dev/dnd/install.clj
@@ -4,7 +4,7 @@
             [clojure.string :as str]))
 
 (defn sh [& args]
-  (println (:out (apply shell/sh args))))
+  (println (:out (apply shell/sh "sh" "-c" args))))
 
 (defn- service-str [env]
   (let [alias (when (= "stage" env) ":test")]
@@ -16,7 +16,7 @@
   (println "Installing Service")
   (let [env (env/env! "ME_ENV")]
     (spit "dnd.service" (service-str env))
-    (sh "sh" "-c" "sudo mv -f dnd.service /etc/systemd/system/dnd.service")
+    (sh "sudo mv -f dnd.service /etc/systemd/system/dnd.service")
     (sh "sudo systemctl daemon-reload")
     (sh "sudo systemctl start dnd")
     (System/exit 0)))
