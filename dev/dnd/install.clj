eb9c7eca66fc458cdccfb5552605469e4106dd83
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2024-07-05 16:54:48 -0500

    Move nginx installation to install script

--- dev/dnd/install.clj
+++ dev/dnd/install.clj
@@ -8,14 +8,39 @@
 
 (defn- service-str [env]
   (let [alias (when (= "stage" env) ":test")]
-    (-> (slurp "./bin/dnd.service")
+    (-> (slurp "./resources/config/dnd.service")
         (str/replace "<:alias>" alias))))
 
-(defn -main []
+(defn- install-service! [env]
   (println "Installing Service")
-  (let [env (env/env! "ME_ENV")]
-    (spit "dnd.service" (service-str env))
-    (sh "sudo mv -f dnd.service /etc/systemd/system/dnd.service")
-    (sh "sudo systemctl daemon-reload")
-    (sh "sudo systemctl start dnd")
+  (spit "dnd.service" (service-str env))
+  (sh "sudo mv -f dnd.service /etc/systemd/system/dnd.service")
+
+  (println "Reloading systemctl")
+  (sh "sudo systemctl daemon-reload")
+
+  (println "Starting App")
+  (sh "sudo systemctl start dnd"))
+
+(defn- site-str [host]
+  (-> (slurp "./resources/config/site.conf")
+      (str/replace "<:host>" host)))
+
+(defn- install-site! [host]
+  (println "Installing Nginx")
+  (sh "sudo apt-get install -y nginx")
+
+  (println "Installing site.conf")
+  (spit "site.conf" (site-str host))
+  (sh "sudo mv -f site.conf /etc/nginx/sites-available/site.conf")
+  (sh "sudo ln -s /etc/nginx/sites-available/site.conf /etc/nginx/sites-enabled/")
+
+  (println "Reloading Nginx")
+  (sh "sudo systemctl reload nginx"))
+
+(defn -main []
+  (let [env  (env/env! "ME_ENV")
+        host (env/env! "HOST")]
+    (install-service! env)
+    (install-site! host)
     (System/exit 0)))
